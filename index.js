54f8ee2ef54c02abe3968f42db26c6e8cf7c2072
RoheithChat <Roheith.class@gmail.com>
2023-05-05 13:27:58 +0530

    main commit

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ const fs = require('fs');
 
 const app = express();
 const port = 3000;
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const searchTerm = urlParams.get('url');
-const videoUrl = searchTerm
+async function getCurrentCopiedText() {
+    try {
+      const clipboardText = await navigator.clipboard.readText();
+      alert(clipboardText)
+    } catch (err) {
+      console.error('Failed to read clipboard content: ', err);
+    }
+  }
+  getCurrentCopiedText();
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/dmf', (req, res) => {
-  
+  const videoUrl = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
   const outputFilePath = './video.mp4';
   const videoStream = ytdl(videoUrl, { filter: 'videoandaudio' });
   videoStream.pipe(fs.createWriteStream(outputFilePath));
@@ -24,7 +30,7 @@ app.get('/dmf', (req, res) => {
 });
 
 app.get('/dmt', (req, res) => {
-  
+  const videoUrl = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
   const outputFilePath = './audio.mp3';
   const audioStream = ytdl(videoUrl, { filter: 'audioonly', format: 'mp3' });
   audioStream.pipe(fs.createWriteStream(outputFilePath));
